4b6b56be0e1863c62e545c3e5930efa9929c70fa
Michael P. Geraci <me@mgeraci.com>
2017-08-20 09:29:20 -0400

    fix text cropping issue on 1/3 fraction

--- src/components/RecipeIngredient.css.js
+++ src/components/RecipeIngredient.css.js
@@ -2,12 +2,12 @@ import { FONT_FAMILY_LIGHT } from "../util/style_consts";
 
 export default {
 	wrapper: {
-		marginTop: 6,
 	},
 
 	text: {
 		fontFamily: FONT_FAMILY_LIGHT,
 		fontSize: 20,
 		lineHeight: 20,
+		paddingTop: 6,
 	},
 };
